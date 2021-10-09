0cd26db1b82fb140e98c57a6a761b4e1921301fc
personqianduixue <3480814463@qq.com>
2021-10-10 02:45:21 +0800

    init11

--- 6国赛LaTeX模板/demo/code/beautiplot3.m
+++
@@ -1,6 +0,0 @@
-function beautiplot3()
-ax =gca;
-set(ax,'XGrid','on','YGrid','on','GridLineStyle','--','GridAlpha',0.15);
-set(ax,'Box','off','LineWidth',1.5);
-set(ax,'XMinorTick','on','YMinorTick','on');
-end
